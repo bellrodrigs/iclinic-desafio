3785f51bc5bba894c424d664124479eecddd1183
Bell <isabelrodrgs@gmail.com>
2022-01-15 22:47:13 -0300

    craeting home page style and create navigation components

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,29 +1,87 @@
 import React, {useState} from 'react';
 import axios from 'axios'
+import styled from 'styled-components'
+import {DarthVader} from '../DarthVader'
+import {LukeSkywalker} from '../LukeSkywalker'
 
 
 export const Home = () => {
-    const [person, setPerson] = useState()
+    const [person, setPerson] = useState(null)
 
     const get = async () => {
       const arr = []
       axios.get('https://swapi.dev/api/people/1').then(async (data) => {
         console.log('d1', data.data.name)
-        await arr.push(data.data.name)
+        await arr.push({id:1 , name: data.data.name})
       });
       
       await axios.get('https://swapi.dev/api/people/4').then(async (data) => {
         console.log('d4', data.data.name)
-        await arr.push(data.data.name)
+        await arr.push({id:4 , name: data.data.name})
       });
   
      setPerson(arr[0])
     }
   
     return (
-      <div className="App">
-        <h2>{person}</h2>
-        <button onClick={() => get()}>aperta ai</button>
-      </div>
+      <Container>
+        {
+            person === null ?
+            <>
+                <div>
+                    <Title>Welcome to <strong>iClinic</strong></Title>
+                    <Subtitle>Frontend Challenge</Subtitle>
+                </div>
+                <StartButton onClick={() => get()}>start</StartButton>
+            </>
+            :
+            <div>
+                {
+                    person.id === 1 && <LukeSkywalker />
+                }
+                {
+                    person.id === 4 && <DarthVader />
+                }
+            </div>
+        }
+        
+      </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+`
+
+const Title = styled.div`
+    font-size: 72px;
+    line-height: 88px;
+    margin-top: 210px;
+    color:#2C97D1;
+
+`
+const Subtitle = styled.div`
+    font-size: 14px;
+    line-height: 17px;
+    color:#2C97D1;
+    text-transform: uppercase;
+    text-align: center;
+ 
+`
+const StartButton = styled.button`
+    text-transform: uppercase;
+    width: 189px;
+    height: 56px;
+    background: #2C97D1;
+    border-radius: 10px;
+    color: #FFFFFF;
+    text-align: center;
+    border: none;
+    font-size: 18px;
+    cursor: pointer;
+    margin-top: 274px;
+`
