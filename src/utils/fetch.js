411dc0c4bc6564ad330ca40d53a1e13d1bc535fb
Bell <isabelrodrgs@gmail.com>
2022-01-16 02:01:52 -0300

    removing console.log

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -3,12 +3,10 @@ import axios from 'axios'
 export const get = async () => {
     const arr = []
     axios.get('https://swapi.dev/api/people/1').then(async (data) => {
-      console.log('d1', data.data.name)
       await arr.push({id:1 , name: data.data.name})
     });
     
     await axios.get('https://swapi.dev/api/people/4').then(async (data) => {
-      console.log('d4', data.data.name)
       await arr.push({id:4 , name: data.data.name})
     });
 
